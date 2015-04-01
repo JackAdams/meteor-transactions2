3f080f7d5114976f432fdcece0976095874b1d8f
Brent Abrahams <brent_abrahams@yahoo.com>
2015-04-01 10:15:42 +0800

    First attempt at a 2-phase commit

---
+++ package.js
@@ -0,0 +1,34 @@
+Package.describe({
+  name: "babrahams:transactions",
+  summary: "Undo/Redo stack based on transactions",
+  version: "0.6.17",
+  git: "https://github.com/jackadams/meteor-transactions.git"
+});
+
+Package.onUse(function (api, where) {
+
+  api.versionsFrom("1.0");
+
+  api.use('jquery', 'client');
+  api.use('tracker', 'client');
+  api.use('minimongo', 'client');
+  api.use('templating', 'client');
+  api.use('spacebars', 'client');
+  api.use('underscore');
+  api.use('mongo');
+  api.use('accounts-base');
+  api.imply('mongo');
+  api.use('dburles:mongo-collection-instances@0.3.3');
+  api.use('aldeed:collection2@2.3.2', ['client','server'], {weak:true});
+
+  api.add_files('lib/transactions_client.html', 'client');
+  api.add_files('lib/transactions_client.js', 'client');
+  api.add_files('lib/transactions_client.css', 'client');
+  api.add_files('lib/transactions_server.js', 'server');
+  api.add_files('lib/transactions_common.js', ['client','server']);
+  
+  if (api.export) {
+    api.export('tx');
+  }
+  
+});
